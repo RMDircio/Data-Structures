2077678b8c8040e07d255cf192fed27b014117cf
Regina Dircio <reginadircio@gmail.com>
2020-09-23 14:39:06 -0500

    Cleaned up :wastebasket:

--- day_2_double_linked_list/doubly_linked_list/doubly_linked_list.py
+++ day_2_double_linked_list/doubly_linked_list/doubly_linked_list.py
@@ -1,179 +1,5 @@
-# ########## Code from Monday Lecture that is helpful 
-# ### noted the functions that can be made inside the delete function
 
-# """
-# Each ListNode holds a reference to its previous node
-# as well as its next node in the List.
-# """
-# class ListNode:
-#     def __init__(self, value, prev=None, next=None):
-#         self.prev = prev
-#         self.value = value
-#         self.next = next
-            
-# """
-# Our doubly-linked list class. It holds references to 
-# the list's head and tail nodes.
-# """
-# class DoublyLinkedList:
-#     def __init__(self, node=None):
-#         self.head = node
-#         self.tail = node
-#         self.length = 1 if node is not None else 0
-
-#     def __len__(self):
-#         return self.length
-    
-#     """
-#     Wraps the given value in a ListNode and inserts it 
-#     as the new head of the list. Don't forget to handle 
-#     the old head node's previous pointer accordingly.
-#     """
-#     def add_to_head(self, value):
-#         # wrap the input value in a node
-#         new_node = ListNode(value)
-#         # increment the length
-#         self.length += 1
-#         # check if the linked list is empty
-#         if not self.head and not self.tail:
-#             # if the list is initially empty, set both head and tail to the new node
-#             self.head = new_node
-#             self.tail = new_node
-#         # we have a non-empty list, add the new node to the head 
-#         else:
-#             # set the new node's `next` to refer to the current head
-#             new_node.next = self.head
-#             # set the current head's 'prev' to refer to the new_node (added to make it work with DLL)
-#             self.head.prev = new_node
-#             # set the list's head reference to the new node 
-#             self.head = new_node
-        
-#     """
-#     Removes the List's current head node, making the
-#     current head's next node the new head of the List.
-#     Returns the value of the removed Node.
-#     """
-#     def remove_from_head(self):
-#         value = self.head.value
-#         self.delete(self.head)
-#         return value
-            
-#     """
-#     Wraps the given value in a ListNode and inserts it 
-#     as the new tail of the list. Don't forget to handle 
-#     the old tail node's next pointer accordingly.
-#     """
-#     def add_to_tail(self, value):
-#         new_node = ListNode(value, None, None)
-#         self.length += 1
-#         if not self.tail and not self.head:
-#             self.tail = new_node
-#             self.head = new_node
-#         else:
-#             new_node.prev = self.tail
-#             self.tail.next = new_node
-#             self.tail = new_node
-            
-#     """
-#     Removes the List's current tail node, making the 
-#     current tail's previous node the new tail of the List.
-#     Returns the value of the removed Node.
-#     """
-#     def remove_from_tail(self):
-#         value = self.tail.value
-#         self.delete(self.tail)
-#         return value
-            
-#     """
-#     Removes the input node from its current spot in the 
-#     List and inserts it as the new head node of the List.
-#     """
-#     def move_to_front(self, node):
-#         if node is self.head:
-#             return
-#         value = node.value
-#         if node is self.tail:
-#             self.remove_from_tail()
-#         else:
-#             if node.prev:
-#                 node.prev.next = node.next
-#             if node.next:
-#                 node.next.prev = node.prev
-
-#             self.length -= 1
-#         self.add_to_head(value)
-        
-#     """
-#     Removes the input node from its current spot in the 
-#     List and inserts it as the new tail node of the List.
-#     """
-#     def move_to_end(self, node):
-#         if node is self.tail:
-#             return
-#         value = node.value
-#         if node is self.head:
-#             self.remove_from_head()
-#         else:
-#             if node.prev:
-#                 node.prev.next = node.next
-#             if node.next:
-#                 node.next.prev = node.prev
-
-#             self.length -= 1
-#         self.add_to_tail(value)
-
-#     """
-#     Deletes the input node from the List, preserving the 
-#     order of the other elements of the List.
-#     """
-#     def delete(self, node):
-#         # check of empty list
-#         if not self.head and not self.tail:
-#             # return nothing
-#             return
-#         # if list is 1 node long
-#         if self.head is self.tail:
-#             self.head = None
-#             self.tail = None
-#         # could make this into a function
-#         elif self.head is node:
-#             self.head = node.next
-#             if node.prev:
-#                 node.prev.next = node.next
-#             if node.next:
-#                 node.next.prev = node.prev
-#         # could make this into a function
-#         elif self.tail is node:
-#             self.tail = node.prev
-#             if node.prev:
-#                 node.prev.next = node.next
-#             if node.next:
-#                 node.next.prev = node.prev
-#         # could make this into a function        
-#         else:
-#             if node.prev:
-#                 node.prev.next = node.next
-#             if node.next:
-#                 node.next.prev = node.prev
-#         self.length -= 1
-
-#     """
-#     Finds and returns the maximum value of all the nodes 
-#     in the List.
-#     """
-#     def get_max(self):
-#         if not self.head:
-#             return None
-#         max_val = self.head.value
-#         current = self.head
-#         while current:
-#             if current.value > max_val:
-#                 max_val = current.value
-#             current = current.next
-#         return max_val
-
-
-
+### FIRST CLASS ###
 """
 Each ListNode holds a reference to its previous node
 as well as its next node in the List.
@@ -187,42 +13,8 @@ class ListNode:
     def __repr__(self):
         return str(self.value)
 
-    # # code from lecture that might be useful for the assignment
-    # ''' 
-    # Wrap the given value in a ListNode and insert it after this node. 
-    # Note that this node could already have a next node it is pointing to.
-    # '''
-    # def insert_after(self,value):
-    #     current_next = self.next
-    #     self.next = ListNode(value, self, current_next)
-    #     if current_next:
-    #         current_next.prev = self.next
 
-    # '''
-    # Wrap the given value in a ListNode and insert it before this node.
-    # Note that this node could already have a previous node it is pointing to
-    # '''
-
-    # def insert_before(self,value):
-    #     current_prev = self.prev
-    #     self.prev = ListNode(value, current_prev, self)
-    #     if current_prev:
-    #         current_prev.next = self.prev
-
-    # '''
-    # Rearranges this ListNode's pervious and next pointers accordingly,
-    # effectively deleting this ListNode
-    # '''
-
-    # def delete(self):
-    #     if self.prev:
-    #         self.prev.next = self.next
-    #     if self.next:
-    #         self.next.prev = self.prev
-
-
-
-
+### SECOND CLASS ###
 """
 Our doubly-linked list class. It holds references to 
 the list's head and tail nodes.
@@ -415,39 +207,39 @@ class DoublyLinkedList(ListNode):
 
 
 # run manual checks
-# noob = DoublyLinkedList()
-# noob.add_to_head(8)
-# noob.add_to_head(4)
-# noob.add_to_head(2)
-# noob.add_to_head(1)
-# print(noob)
+noob = DoublyLinkedList()
+noob.add_to_head(8)
+noob.add_to_head(4)
+noob.add_to_head(2)
+noob.add_to_head(1)
+print(noob)
 
-# # relink node 2 to 8 ( omit 4)
-# print(noob.remove_from_head())
-# print(noob)
+# relink node 2 to 8 ( omit 4)
+print(noob.remove_from_head())
+print(noob)
 
-# # add tail
-# noob.add_to_tail('?')
-# print(noob)
+# add tail
+noob.add_to_tail('?')
+print(noob)
 
-# # remove tail
-# print(noob.remove_from_tail())
-# print(noob)
+# remove tail
+print(noob.remove_from_tail())
+print(noob)
 
-# # move to head
-# print('Last')
-# print('move to front Q')
-# noob.move_to_front(noob.tail)
-# print(noob)
+# move to head
+print('Last')
+print('move to front Q')
+noob.move_to_front(noob.tail)
+print(noob)
 
-# print(noob.delete(noob.head.next))
-# print(noob)
+print(noob.delete(noob.head.next))
+print(noob)
 
-# print('move to end Q')
-# noob.move_to_end(noob.head)
-# print(noob)
+print('move to end Q')
+noob.move_to_end(noob.head)
+print(noob)
 
-# print(noob.get_max())
+print(noob.get_max())
 
 
             
